d366a456bb2e630f94b1b010b0fefc9219d69e51
patilarya76 <patilaryaanil07@gmail.com>
2024-03-04 15:19:27 +0530

    donewithasapdumy

---
+++ client/src/component/homepage.jsx
@@ -0,0 +1,183 @@
+import React from 'react'
+import './homepage.css'
+
+
+function Homepage() {
+  return (
+    <div>
+        <p id='heading'>Lets Explore the Weridest bollywood couples!!</p>
+    <div className='main'>
+      <div id='couple1' className='info'>
+        <img id='image1' src="https://www.bbc.co.uk/staticarchive/1e2864b61456bc1f29c1f935d925c95aa986901e.jpg" alt="" />
+         <p> <b>Couple Names:</b>  Aishwarya Rai and Nana Patekar</p>
+         <p> <b>Reason for weirdness:</b>A significant age gap between the two actors.</p>
+         <p>Movies Together:None.</p>
+         <p>Year of Relationship/Marrigae:Never in a romantic relationship.</p>
+         <p>Current status: Both are married to different people.</p>
+          <p>Controversies/Noble Events:No major controversies surrounding their relationship. </p>
+        </div>
+
+        <div  id='couple2' className='info'>
+       
+        <img id='image2' src="https://www.indiaherald.com/Assets/ArticleUpload/20237220273595_c380a7ee-5b95-4587-b31f-49e52aafe26e.jpg" alt="" />
+        <p> <b>Couple Names:</b> Salman Khan and Aishwarya Rai</p>
+         <p> <b>Reason for weirdness:</b> Their relationship was controversial due to alleged abuse and a messy breakup.</p>
+         <p>Movies Together:"Hum Dil De Chuke Sanam" (1999)."</p>
+         <p>Year of Relationship/Marrigae: Relationship started in the late 1990s.</p>
+         <p>Current status: They are not together; both have moved on.</p>
+          <p>Controversies/Noble Events: Public breakup with allegations of abuse and harassment.</p>
+        </div>
+
+        <div  id='couple3' className='info'>
+        {/* <p>Lets Explore the Weridest bollywood couples!!</p> */}
+        <img id='image3'  src="https://www.masala.com/wp-content/uploads/cloud/2023/09/12/image-117-670x1024.png" alt="" />
+        <p> <b>Couple Names:</b> Rani Mukerji and Govinda</p>
+         <p> <b>Reason for weirdness:</b> Their on-screen pairing was unconventional <br /> due to the significant age gap and different acting styles.</p>
+         <p>Movies Together: "Hadh Kar Di Aapne" (2000), <br /> "Chalo Ishq Ladaaye" (2002), among others.</p>
+         <p>Year of Relationship/Marrigae:Never in a romantic relationship.</p>
+         <p>Current status:Both are married to different people.</p>
+          <p>Controversies/Noble Events:No major <br /> controversies surrounding their pairing.</p>
+        </div>
+
+
+        <div  id='couple4' className='info'>
+        {/* <p>Lets Explore the Weridest bollywood couples!!</p> */}
+        <img  id='image4' src="https://akm-img-a-in.tosshub.com/indiatoday/images/photogallery/201210/kareena_shahid_kapoor_4_650_101612072233.jpg?VersionId=sBG.lWFAS2IDAXfmBNDPobGvldHh5iDD" alt="" />
+        <p> <b>Couple Names:</b> Shahid Kapoor and Kareena Kapoor</p>
+         <p> <b>Reason for weirdness:</b> Despite their breakup, they were cast together in movies post-separation.</p>
+         <p>Movies Together:"Jab We Met" (2007) post-breakup.</p>
+         <p>Year of Relationship/Marrigae:Dated in the early 2000s, broke up in 2007.</p>
+         <p>Current status:Both are married to different people.</p>
+          <p>Controversies/Noble Events:Public breakup and subsequent professional collaborations.</p>
+        </div>
+
+        <div  id='couple5' className='info'>
+        {/* <p>Lets Explore the Weridest bollywood couples!!</p> */}
+        <img id='image5' src="https://st1.bollywoodlife.com/wp-content/uploads/2015/11/631566.jpg" alt="" />
+        <p> <b>Couple Names:</b> Ranbir Kapoor and Deepika Padukone</p>
+         <p> <b>Reason for weirdness:</b> Despite their breakup, they worked together in films.</p>
+         <p>Movies Together:"Yeh Jawaani Hai Deewani" (2013), "Tamasha" (2015).</p>
+         <p>Year of Relationship/Marrigae:Dated in the late 2000s, broke up in 2009.</p>
+         <p>Current status:Both are married to different people.</p>
+          <p>Controversies/Noble Events:Public breakup and subsequent collaborations.</p>
+        </div>
+
+        <div  id='couple6' className='info'>
+        {/* <p>Lets Explore the Weridest bollywood couples!!</p> */}
+        <img id='image6' src="https://static.toiimg.com/photo/60984152.cms" alt="" />
+        <p> <b>Couple Names:</b> Shah Rukh Khan and Juhi Chawla</p>
+         <p> <b>Reason for weirdness:</b>  Despite being close friends, they have portrayed romantic couples on-screen, which some find unconventional.</p>
+         <p>Movies Together: "Darr" (1993), "Yes Boss" (1997), among others.</p>
+         <p>Year of Relationship/Marrigae:Never in a romantic relationship. </p>
+         <p>Current status: Both are married to different people.</p>
+          <p>Controversies/Noble Events: No major controversies surrounding their pairing.</p>
+        </div>
+
+        <div id='couple7' className='info' >
+        {/* <p>Lets Explore the Weridest bollywood couples!!</p> */}
+        <img id='image7' src="https://i.pinimg.com/originals/e3/a0/cf/e3a0cf33213a6d43f2f69ce2812af438.jpg" alt="" />
+        <p> <b>Couple Names:</b> Sanjay Dutt and Madhuri Dixit:</p>
+         <p> <b>Reason for weirdness:</b> They were rumored to have a relationship during the 1990s, which garnered attention due to Sanjay Dutt's legal troubles.</p>
+         <p>Movies Together: "Saajan" (1991).</p>
+         <p>Year of Relationship/Marrigae:Speculated relationship in the early 1990s.</p>
+         <p>Current status:Both are married to different people.</p>
+          <p>Controversies/Noble Events:Rumored relationship during a tumultuous time in Sanjay Dutt's life.</p>
+        </div>
+
+        <div id='couple8' className='info'>
+        {/* <p>Lets Explore the Weridest bollywood couples!!</p> */}
+        <img id='image8' src="https://cdn.siasat.com/wp-content/uploads/2023/07/16alia-ranbir9-819x1024.jpeg" alt="" />
+        <p> <b>Couple Names:</b> Ranbir Kapoor and Alia Bhatt</p>
+         <p> <b>Reason for weirdness:</b> Their relationship garnered attention due to the significant age gap and family dynamics </p>
+         <p>Movies Together:Bhramastra</p>
+         <p>Year of Relationship/Marrigae: Relationship started in the late 2010s.</p>
+         <p>Current status: Still together as of latest reports.</p>
+          <p>Controversies/Noble Events: Media attention due to family connections and age difference.</p>
+        </div>
+
+        <div id='couple9' className='info'>
+        {/* <p>Lets Explore the Weridest bollywood couples!!</p> */}
+        <img id='image9' src="https://img.mensxp.com/media/content/2022/Jan/main-image-r_61e51a6c691bb.jpeg" alt="" />
+        <p> <b>Couple Names:</b> Aksha Kumar and Raveena Tandon</p>
+         <p> <b>Reason for weirdness:</b> They had a high-profile relationship in the 1990s, which was marked by controversies and a messy breakup. </p>
+         <p>Movies Together: "Mohra" (1994), "Khiladiyon Ka Khiladi" (1996), among others.</p>
+         <p>Year of Relationship/Marrigae: Dated in the mid-1990s, broke up in 1999.</p>
+         <p>Current status: Both are married to different people.</p>
+          <p>Controversies/Noble Events: Public relationship with rumors of infidelity and a tumultuous breakup.</p>
+        </div>
+
+        <div id='couple10' className='info'>
+        {/* <p>Lets Explore the Weridest bollywood couples!!</p> */}
+        <img id='image10' src="https://i.pinimg.com/originals/50/1c/3a/501c3a8e9a0f7f1c5bd8a6874e7c8251.jpg" alt="" />
+        <p> <b>Couple Names:</b> Salman Khan and Katrina Kaif</p>
+         <p> <b>Reason for weirdness:</b>  Their relationship was a subject of much speculation and gossip in the media, particularly due to Salman Khan's past relationships and Katrina Kaif's background.</p>
+         <p>Movies Together: "Ek Tha Tiger" (2012), "Tiger Zinda Hai" (2017).</p>
+         <p>Year of Relationship/Marrigae: Rumored relationship in the mid-2000s.</p>
+         <p>Current status: Both have moved on; not together anymore.</p>
+          <p>Controversies/Noble Events: Public relationship with rumors of interference from Salman Khan's family.</p>
+        </div>
+
+        <div id='couple11' className='info'>
+        {/* <p>Lets Explore the Weridest bollywood couples!!</p> */}
+        <img id='image11'
+         src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202312/koffee-with-karan-8-sharmila-tagore-talks-about-saif-ali-khan-amrita-singhs-divorce-284720142-1x1.png?VersionId=Sd.TFnamn_8pGCRqqOzD5yfovqy8nCFv" alt="" />
+       <p> <b>Couple Names:</b> Amrita Singh and Saif Ali Khan</p>
+         <p> <b>Reason for weirdness:</b> Their marriage was unconventional due to the significant age gap and cultural differences.</p>
+         <p>Movies Together: None.</p>
+         <p>Year of Relationship/Marrigae: Married in the early 1990s, divorced in 2004.</p>
+         <p>Current status:Divorced; both have moved on.</p>
+          <p>Controversies/Noble Events: Publicized relationship with a notable age gap and subsequent divorce.</p>
+        </div>
+
+        <div id='couple12' className='info'>
+        {/* <p>Lets Explore the Weridest bollywood couples!!</p> */}
+        <img id='image12' src="https://www.bbc.co.uk/staticarchive/1e2864b61456bc1f29c1f935d925c95aa986901e.jpg" alt="" />
+        <p> <b>Couple Names:</b> Hrithik Roshan and Kangana Ranaut</p>
+         <p> <b>Reason for weirdness:</b>  Their relationship was marked by public feuds, legal battles, and controversy.</p>
+         <p>Movies Together:None.</p>
+         <p>Year of Relationship/Marrigae:Rumored relationship in the mid-2010s.</p>
+         <p>Current status: Legal disputes ongoing; not together.</p>
+          <p>Controversies/Noble Events: Publicized legal battles, allegations of harassment, and public feud.</p>
+        </div>
+
+        <div id='couple13' className='info'>
+        {/* <p>Lets Explore the Weridest bollywood couples!!</p> */}
+        <img id='image13' src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202202/Untitled_design_-_2022-02-18T1_1.png?size=690:388" alt="" />
+        <p> <b>Couple Names:</b> Farhan Akhtar and Adhuna Bhabani</p>
+         <p> <b>Reason for weirdness:</b> Their marriage raised eyebrows due to the unconventional pairing and the fact that Adhuna was older and a hairstylist by profession.</p>
+         <p>Movies Together: None.</p>
+         <p>Year of Relationship/Marrigae: Married in the early 2000s, divorced in 2016.</p>
+         <p>Current status: Divorced; both have moved on.</p>
+          <p>Controversies/Noble Events: Publicized marriage with subsequent divorce and Farhan's relationship with Shibani Dandekar</p>
+        </div>
+
+        <div id='couple14' className='info'>
+        {/* <p>Lets Explore the Weridest bollywood couples!!</p> */}
+        <img id='image14' src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/201207/tina-660_072612113652.jpg?VersionId=L_9Y_ErLizJjnWCKWRfpPGyL3z_rB7p6" alt="" />
+        <p> <b>Couple Names:</b> Rajesh Khanna and Tina Munim </p>
+         <p> <b>Reason for weirdness:</b> Their relationship was controversial due to the significant age gap and Tina's background.</p>
+         <p>Movies Together: "Souten" (1983), "Fifty Fifty" (1981).</p>
+         <p>Year of Relationship/Marrigae: Dated in the late 1970s, broke up in the early 1980s.</p>
+         <p>Current status: Tina Munim is now Tina Ambani, married to Anil Ambani</p>
+          <p>Controversies/Noble Events: Publicized relationship with a notable age gap and subsequent breakup.</p>
+        </div>
+
+        <div id='couple15' className='info'>
+        {/* <p>Lets Explore the Weridest bollywood couples!!</p> */}
+        <img id='image15' src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/201710/rekha-amitabh-story_647_101017122257.jpg?VersionId=EpxYtxMU1IoE9VuOF0wUeLyMdAB5Ds4.&size=690:388" alt="" />
+        <p> <b>Couple Names:</b> Amitabh Bachchan and Rekha</p>
+         <p> <b>Reason for weirdness:</b> Their alleged affair was one of the most talked-about topics in Bollywood due to Amitabh Bachchan's marriage to Jaya Bachchan.</p>
+         <p>Movies Together: "Silsila" (1981), "Do Anjaane" (1976).</p>
+         <p>Year of Relationship/Marrigae: Rumored relationship in the 1970s and 1980s.</p>
+         <p>Current status: Alleged affair ended; both have moved on. </p>
+          <p>Controversies/Noble Events: Publicized alleged affair despite Amitabh Bachchan's marriage.</p>
+        </div>
+
+
+    </div>
+
+  </div>
+  )
+}
+
+export default Homepage
