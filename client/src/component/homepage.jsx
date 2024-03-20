42db74466881c1d987f9519858d50d667ea8bee9
patilarya76 <patilaryaanil07@gmail.com>
2024-03-20 17:43:37 +0530

    reltionadded

--- client/src/component/homepage.jsx
+++ client/src/component/homepage.jsx
@@ -1,37 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import './homepage.css'
-import Navbar from './Nav'
-import axios from 'axios'
-import Card from './Card'
+import React, { useEffect, useState } from 'react';
+import './homepage.css';
+import Navbar from './Nav';
+import axios from 'axios';
+import Card from './Card';
 
 function Homepage() {
-  const [data, setData] = useState([])
-  
+  const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState('all'); // Default to 'all' user
+
+  const getUsers = () => {
+    axios.get("http://localhost:3000/user").then((res) => {
+      setUsers(res.data);
+    });
+  };
+
   const getData = () => {
-    axios.get("https://asap-bollywoodcouples.onrender.com/getdata").then((res) => {
-      setData(res.data)
-    })
-  }
+    axios.get("https://asap-bollywoodcouples.onrender.com/getdata", {
+      params: { user: selectedUser }
+    }).then((res) => {
+      setData(res.data);
+    });
+  };
 
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+    getUsers();
+  }, [selectedUser]); // Fetch data when selectedUser changes
 
+  const filterData=selectedUser==="all"?data:data.filter((el)=>{
+    if (el.user==selectedUser){return el}
+  })
   return (
     <div>
-      <Navbar/>
-      <p id='heading'>Let's Explore the Weirdest Bollywood Couples!!</p>
-      <div className='main'> 
-      <div id='separate'>
-        {data.map((ele, i) => (
-          <Card  key={i} {...ele} getData={getData} />
+      <Navbar />
+      <select id='dropdownmenu' onChange={(e) => setSelectedUser(e.target.value)}>
+        <option value='all'>All Users</option>
+        {users.map((user) => (
+          <option key={user._id} value={user.user}>{user.user}</option>
         ))}
-
-      </div>
-       
+      </select>
+      <p id='heading'>Let's Explore the Weirdest Bollywood Couples!!</p>
+      <div className='main'>
+        <div id='separate'>
+          {filterData.map((ele, i) => (
+            <Card key={i} {...ele} />
+          ))}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
